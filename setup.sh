e656181e7df72e79755a5164ed4831b1a7a9d78f
William Duarte <54290322+williamsduarte@users.noreply.github.com>
2020-06-03 16:45:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,23 +3,18 @@
 # in order to make Laravel able to access
 # cache and logs folder.
 chgrp -R www-data storage bootstrap/cache
-chown -R www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
 chgrp -R www-data storage storage/framework/cache
-chown -R www-data storage storage/framework/cache
 chmod -R 775 storage storage/framework/cache
 
 chgrp -R www-data storage storage/app
-chown -R www-data storage storage/app
 chmod -R 775 storage storage/app
 
 chgrp -R www-data storage storage/framework/views
-chown -R www-data storage storage/framework/views
 chmod -R 775 storage storage/framework/views
 
 chgrp -R www-data storage storage/logs
-chown -R www-data storage storage/logs
 chmod -R 777 storage storage/logs
 
 # Create the symbolic link
